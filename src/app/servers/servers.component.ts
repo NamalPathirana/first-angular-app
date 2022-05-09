d3ca28ce178d4e726d8685a632f171d68519ad7a
Namalpathirana <ntspathirana@gmail>
2022-05-10 00:28:44 +0530

    init angular v0.1

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -7,9 +6,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  newServer:string = 'No server was created!';
+  serverName:string = 'test name';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+      },
+      3000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.newServer = 'New server created. Server Name :'+this.serverName;
+  }
+
+  onChangeServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
